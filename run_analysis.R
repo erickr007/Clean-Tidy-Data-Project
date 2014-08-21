08bb821accdf7545c03244c3427db8cbef5a04a8
erickr007 <erickr007@gmail.com>
2014-08-21 16:17:33 -0700

    updated run analysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 run_analysis <- function(){
 	## Read in features, training, and test data
 	features <- read.csv("features.txt",colClasses="character",sep="",header=FALSE)
-	testX <- read.csv("./test/X_test.txt",colClasses="character", sep="",header=FALSE)
+	testX <- read.csv("./test/X_test.txt",colClasses="numeric", sep="",header=FALSE)
 	testY <- read.csv("./test/Y_test.txt",colClasses="character", sep="",header=FALSE)
-	testSubjects <- read.csv("./test/subject_test.txt",colClasses="character", sep="",header=FALSE)
-	trainX <- read.csv("./train/X_train.txt",colClasses="character", sep="",header=FALSE)
+	testSubjects <- read.csv("./test/subject_test.txt",colClasses="numeric", sep="",header=FALSE)
+	trainX <- read.csv("./train/X_train.txt",colClasses="numeric", sep="",header=FALSE)
 	trainY <- read.csv("./train/Y_train.txt",colClasses="character", sep="",header=FALSE)
-	trainSubjects <- read.csv("./train/subject_train.txt",colClasses="character", sep="",header=FALSE)
+	trainSubjects <- read.csv("./train/subject_train.txt",colClasses="numeric", sep="",header=FALSE)
 
 	## Combine all XY data
 	testSet <- cbind(testY, testSubjects, testX)
@@ -21,7 +21,8 @@ run_analysis <- function(){
 	boolvector <- vector()
 	headindex <- 1
 	
-	while(headindex <= length(heading)){
+	while(headindex <= length(heading))
+	{
 		
 		if(grepl("Activity", heading[headindex], ignore.case=TRUE) | 
 			grepl("Subject", heading[headindex], ignore.case=TRUE) | 
@@ -37,14 +38,27 @@ run_analysis <- function(){
 		headindex <- headindex + 1
 	}	
 	
+	
 	completeSet <- completeSet[,boolvector]
 
 	## Get Activity labels
 	alabels <- read.csv("./activity_labels.txt",colClasses="character",header=FALSE, sep="")
 	names(alabels) <- c("Index","Activity")
 
+	## Replace dataset's Activity integer values with the descriptive labels
+	labelindex <- 1
+	while(labelindex <= length(alabels[,2]))
+	{
+		logicvector <- completeSet[,"Activity"] == alabels[labelindex,1]
+		completeSet[logicvector,"Activity"] = alabels[labelindex,2]	
 	
+		labelindex <- labelindex + 1
+	}
 
-	completeSet
+	## Display average for each activity of each Subject
+	meltDataSet <- melt(completeSet, id=c("Activity","Subject"), measure.vars=names(ds)[3:length(ds)])
+	castDataSet <- dcast(meltds2, Activity + Subject ~ variable, mean)
+
+	castDataSet
 
 }
